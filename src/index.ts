adfb733a6fa33ce4c99721f41f55b021667bfdf1
Cuong Tran <103cuong@gmail.com>
2020-08-23 15:30:51 +0700

    feat: add .gitattributes (#10)

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,24 @@
+import { hera, globalOptions } from 'hera-js';
 import { logger } from 'juno-js';
 
-logger.info('Chao Xìn 🇻🇳');
+globalOptions.url = 'https://graphqlzero.almansi.me/api';
+
+const main = async () => {
+  const { data } = await hera({
+    query: `
+      query {
+        post(id: $id) {
+          id
+          title
+          body
+        }
+      }
+    `,
+    variables: {
+      id: 1
+    }
+  });
+  logger.info(data);
+}
+
+main();
